8ab7cdb45932f37a8d6fa7cd36e29edd65d5f189
ambuakshay <knightmare.akshay@gmail.com>
2016-04-08 22:06:49 +0530

    changed

--- models/MsgSnt.js
+++ models/MsgSnt.js
@@ -2,8 +2,8 @@ var mongoose=require('mongoose');
 var Schema = mongoose.Schema;
 var MessageSentSchema= new Schema({
 	
-	to_phone_no: Number,
-	from_phone_no: Number,
+	to_phone_number: Number,
+	from_phone_number: Number,
 	message: String,
 	time_sent: { type: Date, default: Date.now },
 	time_recieved: { type: Date, default: Date.now }
